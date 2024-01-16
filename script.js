6aaf7e714e063a79f3459712e49c6c6891e7a167
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2024-01-16 14:59:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-//complete this code
+// Class definitions
+
 class Person {
-	constructor(name,age){
-		this._name: name;
-	    this._age: age;
-	}
-	// getter for name
-	get name(){
-		return this._name;
-	}
-	set age(value){
-	 this._age = value;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for age
+  set age(value) {
+    this._age = value;
+  }
 }
 
 class Student extends Person {
-	// constructor(name,age){
-	//    super(name,age);
-	// }
-	study(){
-		console.log(`${this.name} is studying`);
-	}
+  // Constructor for Student class
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method specific to Student class
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
+
 class Teacher extends Person {
-	// constuctor(name,age){
-	// 	super(name, age);
-	// }
-	tech(){
-		console.log(`${this.name} is teaching`);
-	}
+  // Constructor for Teacher class
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method specific to Teacher class
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
-const person = new Person("Akhilesh", 23);
-// console.log(person.name);
-// const student = new Student("Akhilesh", 23);
-// console.log(student.name);
-student.study();
 
-const teacher = new Teacher("Professor Smith", 35);
-// console.log(teacher.name); // Output: Professor Smith
-teacher.teach();
-
-// Do not change the code below this line
+// Assigning classes to the window object
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
